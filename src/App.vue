6fb48582a4d884124b762151ed154db5ff73df4c
Jeremy Hager <jhager@gongos.com>
2018-06-08 17:08:52 -0400

    Signed-off-by: Jeremy Hager <jhager@gongos.com>

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div id="app">
+      <h1>{{ title }}</h1>
+      <NavBar></NavBar>
+    </div>
+    <div id="messageDiv">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import {EventBus} from './components/EventBus.js'
+export default {
+  name: 'app',
+  components: {
+    NavBar: NavBar
+  },
+  data () {
+    return {
+      title: 'Navbar Component Demo',
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+  background: #3A52C9;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+body {
+  background: grey;
+}
+
+#messageDiv {
+  width: 100%;
+  height: 200px;
+  color: white;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="nav-test">
+    <img id="vue-logo" src="./logo.png">
+      Heres a navbar and it's a component!!!
+    <button id="nav_btn1" type="button" class="btn btn-primary" v-on:click.prevent="button1">Button 1</button>
+    <button id="nav_btn2" type="button" class="btn btn-primary" v-on:click.prevent="button2">Button 2</button>
+    <button id="nav_btn3" type="button" class="btn btn-primary" v-on:click.prevent="button3">Button 3</button>
+    <button id="nav_btn4" type="button" class="btn btn-primary" v-on:click.prevent="button4">Button 4</button>
+    <div id="messageDivNav">{{ messageNav }}</div>
+  </div>
+</template>
+
+<script>
+  import {EventBus} from './EventBus.js'
+  export default {
+    data () {
+      return {
+        count: 0,
+        messageNav: 'Click a button'
+      }
+    },
+    methods: {
+      button1: function() {
+        this.messageNav = 'Button 1 was just clicked!!';
+        // EventBus.$emit({message: self.$data.messageNav});
+      },
+      button2: function() {
+        this.messageNav = 'Button 2 was just clicked!!';
+
+      },
+      button3: function() {
+        this.messageNav = 'Button 3 was just clicked!!';
+
+      },
+      button4: function() {
+        this.messageNav = 'Button 4 was just clicked!!';
+
+      }
+    }
+  }
+</script>
+
+<style>
+#vue-logo {
+  height: 50px;
+}
+</style>
